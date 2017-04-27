3c4676869edd6fe21e8e9f2838761f83ecefeb20
Aleksey Matsarski <matsarskialiaksei@yahoo.com>
2017-04-26 20:22:20 -0700

    Add assertTrue

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -12,6 +14,7 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 		System.out.println("Test Success 12");
+		assertTrue(true);
 	}
 
 }
